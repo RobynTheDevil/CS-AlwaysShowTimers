64ac7c1f59acd6918d284acbdeb79a0a56e28cb0
RobynTheDevil <fortytwopirates@gmail.com>
2024-03-26 05:22:06 -0400

    auto update cards

--- AlwaysShowTimers.cs
+++ AlwaysShowTimers.cs
@@ -1,17 +1,24 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using SecretHistories;
+using SecretHistories.UI;
+using SecretHistories.Manifestations;
+using SecretHistories.Entities;
+using SecretHistories.Spheres;
+using SecretHistories.Abstract;
 using HarmonyLib;
 
 public class AlwaysShowTimers : MonoBehaviour
 {
     public static PatchTracker showTimers {get; private set;}
+    public static SettingTrackerUpdate whenUpdated = WhenSettingUpdated;
 
     public void Start() {
         try
         {
-            AlwaysShowTimers.showTimers = new PatchTracker("AlwaysShowTimers", new MainPatch());
+            AlwaysShowTimers.showTimers = new PatchTracker("AlwaysShowTimers", new MainPatch(), AlwaysShowTimers.whenUpdated);
         }
         catch (Exception ex)
         {
@@ -27,5 +34,37 @@ public class AlwaysShowTimers : MonoBehaviour
         NoonUtility.Log("AlwaysShowTimers: Initialised");
 	}
 
+    public static IEnumerable<ElementStack> GetCards() {
+        return Watchman.Get<HornedAxe>().GetExteriorSpheres()
+            .Where<Sphere>((Func<Sphere, bool>) (x => (double) x.TokenHeartbeatIntervalMultiplier > 0.0))
+            .SelectMany<Sphere, Token>((Func<Sphere, IEnumerable<Token>>) (x => x.GetTokens()))
+            .Select<Token, ITokenPayload>((Func<Token, ITokenPayload>) (x => x.Payload))
+            .OfType<ElementStack>()
+            .Where<ElementStack>((Func<ElementStack, bool>) (x => (x.Decays)));
+    }
+
+    public static void WhenSettingUpdated(object _) {
+        if (AlwaysShowTimers.showTimers.current) {
+            AlwaysShowTimers.Enable();
+        } else {
+            AlwaysShowTimers.Disable();
+        }
+    }
+
+    public static void Enable() {
+        IEnumerable<ElementStack> cards = AlwaysShowTimers.GetCards();
+        foreach (ElementStack card in cards) {
+            Traverse.Create(card).Field("_alwaysDisplayDecayView").SetValue(true);
+            Traverse.Create(card).Field("_token").Field("_manifestation").Method("ShowDecayView").GetValue();
+        }
+    }
+
+    public static void Disable() {
+        IEnumerable<ElementStack> cards = AlwaysShowTimers.GetCards();
+        foreach (ElementStack card in cards) {
+            Traverse.Create(card).Field("_alwaysDisplayDecayView").SetValue(false);
+            Traverse.Create(card).Field("_token").Field("_manifestation").Method("HideDecayView").GetValue();
+        }
+    }
 }
 
--- Patches.cs
+++ Patches.cs
@@ -6,18 +6,21 @@ using System.Linq;
 using SecretHistories.Manifestations;
 using HarmonyLib;
 
-[HarmonyPatch(typeof(CardManifestation), nameof(CardManifestation.HideDecayView))]
+[HarmonyPatch(typeof(CardManifestation), nameof(CardManifestation.Initialise))]
 public class MainPatch : Patch
 {
     public MainPatch() {
-        this.original = AccessTools.Method(typeof(CardManifestation), "HideDecayView");
+        this.original = AccessTools.Method(typeof(CardManifestation), "Initialise");
         this.patch = AccessTools.Method(typeof(MainPatch), "Transpiler");
     }
 
     public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
     {
         var codes = new List<CodeInstruction>(instructions);
-        codes.Insert(0, new CodeInstruction(OpCodes.Ret));
+        int ind = PatchHelper.FindLdstrOperand(codes, "CardManifestation_");
+        codes.Insert(ind - 2, new CodeInstruction(OpCodes.Ldarg_0));
+        codes.Insert(ind - 1, new CodeInstruction(OpCodes.Ldc_I4_1));
+        codes.Insert(ind, new CodeInstruction(OpCodes.Stfld, AccessTools.Field(typeof(CardManifestation), "_alwaysDisplayDecayView")));
         return codes.AsEnumerable();
     }
 
